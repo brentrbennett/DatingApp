dc2ba043749bcbf2aab3eaeeb64bed7265e3c4bb
brent <brentrbennett@yahoo.com>
2019-04-18 01:23:10 -0700

    Initial Commit

---
+++ Models/Value.cs
@@ -0,0 +1,13 @@
+namespace DatingApp.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public Value()
+        {
+            
+        }
+    }
+}
